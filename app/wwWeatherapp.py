eba414e5f31e26a92d80afaabb13105bb949bc5e
WWuerthner <Warren@Warrens-MBP.home>
2017-08-06 17:05:01 -0400

    Put modules into function form and added funwithweather

--- app/wwWeatherapp.py
+++ app/wwWeatherapp.py
@@ -56,21 +56,88 @@ def clothingreco():
     print("\n")
     print("-------------------------")
 
+# ADDING SOME FUN FUNCTIONALITY WITH PICTURES FOR A FEW POSSIBLE CONDITIONS (NOT ALL CONDITIONS AS THE LIST OF POSSIBILITIES IS PROHIBITIVE TO FIND PICTURES FOR)
+
+def funwithweather():
+    if conditions["text"] in {"Sunny","Mostly Sunny"}:
+## credit to http://www.ascii-art.de/ascii/s/sun.txt Philip Kaulfuss
+        print("""
+                  .     :     .
+            .  :    |    :  .
+             .  |   |   |  ,
+              \  |     |  /
+          .     ,-'''''`-.     .
+            "- /  __ __  \ -"
+              |==|  I  |==|
+        - --- | _`--^--'_ | --- -
+              |'`.     ,'`|
+            _- \  "---"  / -_
+          .     `-.___,-'     .
+              /  |     |  |
+            .'  |   |   |  `.
+               :    |    :
+              .     :     .
+             """)
+
+## Credit to http://www.ascii-art.de/ascii/my/small3.shtml
+    elif conditions["text"] in {"Partly Cloudy", "Mostly Cloudy", "Cloudy"}:
+        print("""
+
+            .               /
+      \       I
+                  /
+        \  ,g88R_
+          d888(`  ).                   _
+ -  --==  888(     ).=--           .-(`  )`.
+)         Y8P(       '`.          :(   .    )
+        .+(`(      .   )     .--  `.  (    ) )
+       ((    (..__.:'-'   .=(   )   ` _`  ) )
+`.     `(       ) )       (   .  )     (   )  ._
+  )      ` __.:'   )     (   (   ))     `-'.+(`  )
+)  )  ( )       --'       `- __.'         :(      ))
+.-'  (_.'          .')                    `(    )  ))
+                  (_  )                     ` __.:'
+                  """)
+
+    elif conditions["text"] in {"Showers", "Thunderstorms", "Scattered Thunderstorms", "Scattered Showers", "Rain"}:
+## Credit to Keith R. Fulton | keith.fulton@chinalake.navy.mil
+## http://www.ascii-art.de/ascii/pqr/rain.txt
+
+        print("""
+       _
+     _( )_          _
+   _(     )_      _( )_
+  (_________)   _(     )_
+    \  \  \    (_________)
+      \  \       \  \  \
+                   \  \
+
+""")
+
+
 # PRINT CURRENT WEATHER CONDITIONS
-print("\n")
-print(weather["title"])
-print("Latitude: ", weather["lat"])
-print("Longitude: ", weather["long"])
-print("\n")
-print("Currently it is",conditions["text"],"and",conditions['temp'],"degrees outside!")
-print("\n")
+def currentweather():
+    print("\n")
+    print(weather["title"])
+    print("Latitude: ", weather["lat"])
+    print("Longitude: ", weather["long"])
+    print("\n")
+    print("Currently it is",conditions["text"],"and",conditions['temp'],"degrees outside!")
+    print("\n")
+
+currentweather()
+
+funwithweather()
 
 clothingreco()
 
 # PRINT WEATHER FORECAST
-print("\n")
-print("10 DAY FORECAST:")
-print("\n")
-for d in forecast:
-    print("The forecast for", d["day"], d["date"], "is", d["text"], "with a high of", d["high"], "and a low of", d["low"])
-print("\n")
+def weatherforecast():
+    print("\n")
+    print("10 DAY FORECAST:")
+    print("\n")
+    for d in forecast:
+        print("The forecast for", d["day"], d["date"], "is", d["text"], "with a high of", d["high"], "and a low of", d["low"])
+    print("\n")
+
+weatherforecast()
